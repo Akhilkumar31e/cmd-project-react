86c33345fc1b868756abe2b462943f7f8d73850d
Himadri21 <himadri.khanchandani@gmail.com>
2020-10-09 16:38:27 +0530

    RRR and updatedDate functionality added

--- cmd-project-react/src/main/java/com/virtusa/demo/repository/DeviceRepository.java
+++ cmd-project-react/src/main/java/com/virtusa/demo/repository/DeviceRepository.java
@@ -1,12 +1,13 @@
 package com.virtusa.demo.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.virtusa.demo.model.Device;
 
 public interface DeviceRepository extends JpaRepository<Device, Long> {
-	List<Device> findByDeviceStatus(String deviceStatus);
+	Optional<Device> findByDeviceStatus(String deviceStatus);
 	List<Device> findByDeviceName(String deviceName);
 }
